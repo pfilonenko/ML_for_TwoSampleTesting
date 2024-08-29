d408ab9642090ac2d6b0fd6e5b4ba2cb3cd59a6f
Petr Philonenko <70382810+pfilonenko@users.noreply.github.com>
2024-08-29 14:56:15 +0300

    Add files via upload

--- proposed_ml_for_two_sample_testing/source/ML_TwoSample_Method.py
+++ proposed_ml_for_two_sample_testing/source/ML_TwoSample_Method.py
@@ -693,4 +693,4 @@ if __name__ == '__main__':
             S_WKM               = WeightedKaplanMeier_Test().statistic(A, B),
         )
 
-        print(f'Sn: {Sn}, p-value: {pvalue}, avg_error: {avg_error}')
+        print('Sn: {:.4}, p-value: {:.3}±{:.3}'.format(Sn,pvalue,avg_error))
